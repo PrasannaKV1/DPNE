2d62a07b8d58234bf72f8e3015f63c1e398d119b
PrasannaKV1 <prasannapooja215@gmail.com>
2025-05-27 15:31:20 +0530

    this is the main question

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DPNE
 All the things related to Node Express MangoDB
+this nodejs express mangodb 
+All the nodejs, notes,, 
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import express from 'express';
+const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
